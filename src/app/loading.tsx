d351657b689e20695e62981876cea697a181fa26
TK <121897808+tookarius@users.noreply.github.com>
2026-02-13 09:23:30 +0300

    Delete src directory

--- src/app/loading.tsx
+++
@@ -1,9 +0,0 @@
-import Spinner from './components/Spinner';
-
-export default function RootLoading() {
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      <Spinner size={6} />
-    </div>
-  );
-} 
