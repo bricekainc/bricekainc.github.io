3a4ea9370eec60cc33089c02c7e448b41400308f
TK <121897808+tookarius@users.noreply.github.com>
2026-05-07 14:16:26 +0300

    Update trimdprocess.php

--- scripts/trimdprocess.php
+++ scripts/trimdprocess.php
@@ -1,10 +1,8 @@
 <?php
-require_once 'config.php';
-
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['url'])) {
     $url = $_POST['url'];
     
-    $ch = curl_init("{$config['site_url']}/api/url/add");
+    $ch = curl_init("https://trimd.cc/84732315126ed4f801f108a35e6fd710/url/add");
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_POST, true);
     curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode([
